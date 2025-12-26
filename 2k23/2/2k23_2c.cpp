763917025a548b9dcedae21a12fba645add9b2a5
Ritovash Chanda <ritovash5091@gmail.com>
2025-12-27 01:49:14 +0600

    Update 2k23_2c.cpp

--- 2k23/2/2k23_2c.cpp
+++ 2k23/2/2k23_2c.cpp
@@ -1,3 +1,5 @@
+//Roll-cgpa input-output into text file
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -7,11 +9,31 @@ class Student
     float cgpa;
 
 public:
-    Student(int r, float c) : roll(r), cgpa(c) {}
-    void write_to_disk(string)
+    Student(){}
+    void write_to_disk(const string& file_name)
     {
+        cin>>roll>>cgpa;
+        ofstream fout(file_name, ios::app);
+        fout<<roll<<" "<<fixed<<setprecision(2)<<cgpa<<endl;
+        fout.close();
     }
-    void read_from_disk(string, int sn)
+    void read_from_disk(const string& file_name, int sn)
     {
+        ifstream fin(file_name);
+        int count=0;
+        while(fin>>roll>>cgpa){
+            count++;
+            if(count==sn){
+                cout<<"Stud "<<sn<<" Roll:"<<roll<<" Cgpa:"<<fixed<<setprecision(2)<< cgpa<<endl;
+                break;
+            }
+        }
+        fin.close();
     }
+};
+int main(){
+    Student s;
+    s.write_to_disk("stud.txt");
+    s.read_from_disk("stud.txt",2);
+    return 0;
 }
