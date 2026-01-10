ae60ce36ae46f3cc9ef06bb9f138a3507d7afd0c
Ritovash Chanda <ritovash5091@gmail.com>
2026-01-10 23:56:27 +0600

    Update main.cpp

--- 2k23/7/main.cpp
+++ 2k23/7/main.cpp
@@ -1,22 +1,44 @@
-#include <iostream>
+#include <bits/stdc++.h>
 #include "Complex.h"
-#include "Matrix.h"
-
 using namespace std;
 
+template<class T>
+class Matrix{
+    T data[2][2];
+    public:
+    friend istream& operator>>(istream &in,Matrix<T> &M){
+        for(int i=0;i<2;i++){
+            for (int j=0;j<2;j++){
+                in>>M.data[i][j];
+            }
+        }
+        return in;
+    }
+    friend ostream& operator<<(ostream &out,Matrix<T> &M){
+        for(int i=0;i<2;i++){
+            for (int j=0;j<2;j++){
+                out<<M.data[i][j];
+            }
+            out<<endl;
+        }
+        return out;
+    }
+    Matrix<T> operator+(Matrix<T> &M) {
+        Matrix<T> temp;
+        for(int i=0;i<2;i++) {
+            for(int j=0;j<2;j++) {
+                temp.data[i][j] = data[i][j] + M.data[i][j];
+            }
+        }
+        return temp;
+    }
+};
 int main() {
-    Matrix<Complex> A, B, C;
-
-    cout << "Enter elements of first 2x2 complex matrix:\n";
-    A.input();
-
-    cout << "\nEnter elements of second 2x2 complex matrix:\n";
-    B.input();
-
-    C = A + B;
-
-    cout << "\nResultant Matrix after Addition:\n";
-    C.display();
-
+    Matrix<Complex> m1,m2;
+    cin>>m1;
+    cout<<m1<<endl;
+    cin>>m2;
+    cout<<m2<<endl;
+    cout<<m1+m2<<endl;
     return 0;
 }
